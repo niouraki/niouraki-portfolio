1f13cb71c591a6ac7ad6b5bb65e97ce2bb98e5af
Charikleia Niouraki <niouraki@gmail.com>
2019-03-08 22:30:21 +0200

    update portfolio

--- js/app.js
+++ js/app.js
@@ -1,18 +1,19 @@
 let about = document.getElementById('about-button');
 let portfolio = document.getElementById('portfolio-button');
 let contact = document.getElementById('contact-button');
+let arrow = document.getElementById('arrow-button');
 
 //handle smooth scrolling
 about.addEventListener('click', function() {
   window.scroll({
-    top: 420,
+    top: 500,
     behavior: "smooth"
   });
 });
 
 portfolio.addEventListener('click', function() {
   window.scroll({
-    top: 780,
+    top: 1200,
     behavior: "smooth"
   });
 });
@@ -20,6 +21,13 @@ portfolio.addEventListener('click', function() {
 contact.addEventListener('click', function() {
   window.scroll({
     top: 3000,
+    behavior: "smooth"
+  });
+});
+
+arrow.addEventListener('click', function() {
+  window.scroll({
+    top: 0,
     behavior: "smooth"
   });
 });
